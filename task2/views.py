900a048086f8fed1f47a11baedcd52e3111e00bb
Govind Thakur <govind.thakur11@gmail.com>
2019-08-23 13:35:14 +0530

    Task2

---
+++ task2/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+import json
+import time
+
+# Create your views here.
+def home(request):
+    url= "https://api.spacexdata.com/v3/launches"
+    response=requests.get(url)
+    test=response.json()
+
+    flights=[]
+    for i in test:
+        dict={}
+        dict = {'FlightNumber': i['flight_number'], 'Rocket': i['rocket']['rocket_name'],'links': i['links']['mission_patch'], 'Time': time.strftime("%D %H:%M",time.localtime(i['launch_date_unix']))}
+        flights.append(dict)
+    print (flights)
+    return HttpResponse("<p><h4>{}</h4></p>".format(fl) for fl in flights)
